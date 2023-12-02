fed6f6a4e07d491a14f5d477606e2fe7f388cf4d
Ezhil6999 <ezhil6999@gmail.com>
2023-12-02 23:03:03 +0530

    added txt file and some addede features

--- projects/lib/main.dart
+++ projects/lib/main.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:projects/phone.dart';
-import 'package:projects/verify.dart';
+import 'package:projects/constants/global_variables.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: 'phone',
-    debugShowCheckedModeBanner: false,
-    routes: {
-      'phone': (context) => MyPhone(),
-      'verify': (context) => MyVerify(),
-      'otp':(context) => MyOtp()
-    },
-  ));
+  runApp(const Main());
+}
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor, // will decorate the body without the app bar
+        appBarTheme: const AppBarTheme(                             // decorating the app bar with theme
+           elevation: 0,                                            
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        )
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello'),
+        ),
+        body: const Center(
+          child: Text("hi hello") ,
+        )
+        )
+
+    );
+  }
 }
